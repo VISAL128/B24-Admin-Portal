e4b002d6f7954c511a2b8aa45fe274c4410876ea
Rean Makara <r.makara232@gmail.com>
2025-06-28 11:15:02 +0700

    apply page transitions

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,18 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/fonts', '@nuxt/ui', '@nuxt/icon', 'nuxt-charts']
+  modules: ['@nuxt/fonts', '@nuxt/ui', '@nuxt/icon', 'nuxt-charts'],
+  app: {
+    head: {
+      title: 'Bill24 Admin Portal',
+      meta: [
+        { name: 'description', content: 'Bill24 Admin Portal' }
+      ]
+    },
+    pageTransition: {
+      name: 'page',
+      mode: 'out-in',
+      // duration: 300
+    }
+  }
 })
