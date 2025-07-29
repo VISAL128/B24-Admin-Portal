921d41078282ec3076e8d072b4ef4129469c1200
Rean Makara <r.makara232@gmail.com>
2025-07-29 16:39:00 +0700

    update

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 SET DOCKER_DEFAULT_PLATFORM=linux/amd64
 set NODE_OPTIONS=--no-deprecationdoc
 npm run build
-docker build -t registry.bill24.io/b24/admin-portal:beta-1.9 .
-docker push registry.bill24.io/b24/admin-portal:beta-1.9
+docker build -t registry.bill24.io/b24/admin-portal:beta-1.0.7 .
+docker push registry.bill24.io/b24/admin-portal:beta-1.0.7
---
+++ publish.bat
@@ -0,0 +1,13 @@
+SET IMAGE_TAG=beta-1.0.7
+SET IMAGE_NAME=registry.bill24.io/b24/admin-portal
+
+npm run build
+@REM Remote to Staging server
+SET DOCKER_HOST=tcp://172.16.81.141:2375
+@REM echo "Building Docker image for admin portal..."
+SET DOCKER_DEFAULT_PLATFORM=linux/amd64
+docker build -t %IMAGE_NAME%:%IMAGE_TAG% .
+docker push %IMAGE_NAME%:%IMAGE_TAG%
+@REM Deploying application to staging...
+docker compose -p b24-stage up --pull "always" --detach admin_portal
+@REM Deployment to staging completed.
